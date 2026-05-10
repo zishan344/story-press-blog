22924c65a2a2f730afdc7c10ad6ea36548ea323c
zishan344 <zishanahmed344@gmail.com>
2026-05-10 13:21:47 +0600

    refactor: comment out unused DNS server configuration in db.ts

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,7 +1,6 @@
 import mongoose from 'mongoose';
-import dns from 'node:dns';
 import { env } from './env';
-dns.setServers(['8.8.8.8', '1.1.1.1']);
+// dns.setServers(['8.8.8.8', '1.1.1.1']);
 export const connectDB = async () => {
   mongoose.set('strictQuery', true);
   try {
